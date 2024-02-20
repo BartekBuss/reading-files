f841c780c17b933d9dfcb5361482fe5700b3f2ec
BartekBuss <129660846+BartekBuss@users.noreply.github.com>
2024-02-20 19:04:42 +0100

    Add files via upload

---
+++ Otwieranie plików/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Otwieranie_plików
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.wyborPliku = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineWidth = 2;
+            this.chart1.CausesValidation = false;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.IsSoftShadows = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(299, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsVisibleInLegend = false;
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.White;
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsVisibleInLegend = false;
+            series2.IsXValueIndexed = true;
+            series2.LabelForeColor = System.Drawing.Color.Maroon;
+            series2.Legend = "Legend1";
+            series2.Name = "linia";
+            series2.ShadowColor = System.Drawing.Color.Yellow;
+            series2.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(873, 637);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(23, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 265);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Otwórz plik";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wyborPliku
+            // 
+            this.wyborPliku.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(23, 503);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 74);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Czyszczenie wykresu";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1031, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1031, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1031, 99);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1200, 700);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog wyborPliku;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Otwieranie plików/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Otwieranie_plików
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog wyborPliku = new OpenFileDialog
+            {
+                InitialDirectory = @"c:\desktop",
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+            };
+            
+            wyborPliku.ShowDialog();
+            string plik = wyborPliku.FileName;
+
+            //string plik = @"C:\Users\Bartosz\Desktop\1.txt";
+
+            chart1.Series["Series1"].Points.Clear();
+            chart1.Series["linia"].Points.Clear();
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+
+            string[] daneDoWykresu = new string[2];
+            float max = 0;
+            float xMax = 0;
+            float halfMax = 0;
+            float x1 = 0;
+            float x2 = 0;
+            float y1 = 0;
+            float y2 = 0;
+            float a = 0;
+            float b = 0;
+            float x1Hmax = 0;
+            float x2Hmax = 0;
+            float FWHM;
+
+            foreach (string wiersz in File.ReadLines(plik))
+            {
+                string[] podzielone = wiersz.Split(' ');
+
+                daneDoWykresu[0] = podzielone[0].Replace('.', ',');
+                daneDoWykresu[1] = podzielone[1].Replace('.', ',');
+
+                if (Math.Round(float.Parse(daneDoWykresu[1]), 5) > max)
+                {
+                    max = float.Parse(daneDoWykresu[1]);
+                    xMax = float.Parse(daneDoWykresu[0]);
+                }
+
+                halfMax = max / 2;
+
+                if (Math.Round(float.Parse(daneDoWykresu[1]), 5) < max & (Math.Round(float.Parse(daneDoWykresu[1]), 5) > halfMax) & (Math.Round(float.Parse(daneDoWykresu[0]), 5)) > xMax)
+                {
+                    y2 = float.Parse(daneDoWykresu[1]);
+                    x2 = float.Parse(daneDoWykresu[0]);
+                }
+
+                if (Math.Round(float.Parse(daneDoWykresu[1]), 5) > 20000  & (Math.Round(float.Parse(daneDoWykresu[1]), 5) < halfMax) & (Math.Round(float.Parse(daneDoWykresu[0]), 5)) > xMax)
+                {
+                    y1 = float.Parse(daneDoWykresu[1]);
+                    x1 = float.Parse(daneDoWykresu[0]);
+                }
+
+                chart1.Series["Series1"].Points.AddXY(Math.Round(float.Parse(daneDoWykresu[0]),5), Math.Round(float.Parse(daneDoWykresu[1]),5));
+            }
+
+            foreach (string wiersz in File.ReadLines(plik))
+            {
+                string[] podzielone = wiersz.Split(' ');
+
+                daneDoWykresu[0] = podzielone[0].Replace('.', ',');
+                daneDoWykresu[1] = podzielone[1].Replace('.', ',');
+
+                chart1.Series["linia"].Points.AddXY(Math.Round(float.Parse(daneDoWykresu[0]), 5), halfMax);
+            }
+
+            a = (y2 - y1) / (x2 - x1);
+            b = y1 - a * x1;
+            x2Hmax = (halfMax - b) / a;
+
+            x1Hmax = xMax - (x2Hmax - xMax);
+
+            FWHM = (x2Hmax - xMax) * 2;
+
+            textBox1.Text = "FWHM = " + FWHM.ToString();
+            textBox2.Text = "x1 = " + x1Hmax.ToString();
+            textBox3.Text = "x2 = " + x2Hmax.ToString();  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Series1"].Points.Clear();
+            chart1.Series["linia"].Points.Clear();
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+        }
+    }
+}
